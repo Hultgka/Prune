264cf6cdf5d74679ae8061c501f7ef19fd71e29a
Hultgka <karissa.hultgren@gmail.com>
2025-07-23 11:53:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2670,3 +2670,22 @@ themePicker.addEventListener('change', () => {
   localStorage.setItem('plantTheme', themePicker.value);
   // You could update the sidebar plant here based on themePicker.value!
 });
+document.getElementById('testBreakNotificationBtn').onclick = function() {
+  if ('Notification' in window) {
+    if (Notification.permission === "granted") {
+      new Notification("Test notification from Prune!", { body: "You should see this message." });
+    } else if (Notification.permission === "default") {
+      Notification.requestPermission().then(function(permission) {
+        if (permission === "granted") {
+          new Notification("Test notification from Prune!", { body: "Permission granted and this message should appear." });
+        } else {
+          alert("Notification permission was denied.");
+        }
+      });
+    } else {
+      alert("Notifications are blocked for this site.");
+    }
+  } else {
+    alert("This browser does not support notifications.");
+  }
+};
