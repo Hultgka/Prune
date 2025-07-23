daf14f3a76141ee06d1618b60ab08c444cea5743
Hultgka <karissa.hultgren@gmail.com>
2025-07-23 13:15:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2670,33 +2670,3 @@ themePicker.addEventListener('change', () => {
   localStorage.setItem('plantTheme', themePicker.value);
   // You could update the sidebar plant here based on themePicker.value!
 });
-
-console.log("script.js loaded");
-
-const testBtn = document.getElementById('testBreakNotificationBtn');
-console.log("testBreakNotificationBtn found:", !!testBtn);
-
-if (testBtn) {
-  testBtn.onclick = function() {
-    console.log("Test Notification button clicked");
-    if ('Notification' in window) {
-      if (Notification.permission === "granted") {
-        new Notification("Test notification from Prune!", { body: "You should see this message." });
-      } else if (Notification.permission === "default") {
-        Notification.requestPermission().then(function(permission) {
-          if (permission === "granted") {
-            new Notification("Test notification from Prune!", { body: "Permission granted and this message should appear." });
-          } else {
-            alert("Notification permission was denied.");
-          }
-        });
-      } else {
-        alert("Notifications are blocked for this site.");
-      }
-    } else {
-      alert("This browser does not support notifications.");
-    }
-  };
-} else {
-  console.log("testBreakNotificationBtn is missing from the DOM.");
-}
